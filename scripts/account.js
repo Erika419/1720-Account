cbf031129312aad9863ca286354b57e932fadb0a
Erika Costecalde <11122171@uvu.edu>
2026-09-22 14:18:50 -0600

    added things to account.js

--- scripts/account.js
+++ scripts/account.js
@@ -1,11 +1,13 @@
 // Grab references to sign up form
 const signup = document.querySelector("#signup");
 const submitBtn = document.querySelector("#signup input[type=submit]");
+submitBtn.addEventListener('click', createProfile);
 
 //Grab references to the delete local storage
 const display = document.querySelector("#display");
 const details = document.querySelector("#display div");
 const deleteBtn = document.querySelector("#display > button"); 
+deleteBtn.addEventListener("click", deleteProfile);
 
 //Check if local data storage already exists
 const myaccount = localStorage.getItem("ly-Name");
@@ -16,4 +18,14 @@ if(myaccount === null){
 } else{
     signup.classList.add("hide");
     signup.classList.remove("hide")
+}
+
+//Funtion to create a new profile
+function createProfile(){
+    console.log("You created a new Profile")
+}
+
+//Funtion to delete a new profile
+function deleteProfile(){
+    console.log("You DELETED a Profile")
 }
